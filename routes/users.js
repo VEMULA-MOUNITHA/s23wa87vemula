fb990a004ac924f232df647538dff5c50db84001
VEMULA-MOUNITHA <vemulamounitha09@gmail.com>
2023-03-28 22:10:12 -0500

    edited users pug file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var  sum=0;
+var times =0;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+ 
+  times+=1;
+  sum+=times;
+  res.send('Sum is:'+sum);
 });
 
 module.exports = router;
