5fbe19aa9b74f06ab7e00cc5c4d4b5b256b8191c
VEMULA-MOUNITHA <vemulamounitha09@gmail.com>
2023-03-29 14:20:15 -0500

    done changes

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var  sum=0;
-var times =0;
+var more =0;
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
  
-  times+=1;
-  sum+=times;
+  more+=1;
+  sum+=more;
   res.send('Sum is:'+sum);
 });
 
